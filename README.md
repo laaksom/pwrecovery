f1b646673e66401cc221de317360399aa92e1da6
laakso <matias.laakso@protonmail.com>
2022-05-08 12:59:08 +0300

    Generate variants for initial password guess

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # pwrecovery
+
+Openssl library is used when calculating hashes with CPU. On Linux install the necessary library with:
+```
+sudo apt-get install libssl-dev
+```
+
+And then compile the program with lcrypto-flag:
+```
+g++ main.cpp -o pwrecovery -lcrypto
+```
+
--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <sstream>
 #include <string>
 #include <fstream>
+#include <cctype>
+#include <vector>
+#include <map>
 
 
 #include <openssl/sha.h>
@@ -70,11 +73,93 @@ void searchFromPasswordFile(std::string inputHash)
     return;
 }
 
+void generateVariants(const std::string original, std::vector<std::string> &variants)
+{
+    variants.push_back(original);
+    // Add version with first letter capitalized
+    std::string modifiedString = original;
+    modifiedString[0] = std::toupper(modifiedString[0]);
+    variants.push_back(modifiedString);
+
+    // Replace letters with numbers
+    std::map<char, char> charAsNumber = {{'a', '4'}, {'e', '3'}, {'i', '1'}, {'o', '0'}, {'t', '7'}, {'s', '5'}};
+
+    int originalCount = variants.size();
+    for (int i = 0; i < originalCount; ++i)
+    {
+        modifiedString = variants[i];
+        for (int j = 0; j < modifiedString.length(); ++j)
+        {
+            if (charAsNumber.find(modifiedString[j]) != charAsNumber.end())
+            {
+                modifiedString[j] = charAsNumber.at(modifiedString[j]);
+                variants.push_back(modifiedString);
+            }
+        }
+    }
+
+    // Add 1, 12, 123 to ends of variants
+    originalCount = variants.size();
+    for (int i = 0; i < originalCount; ++i)
+    {
+        modifiedString = variants[i];
+        modifiedString.push_back('1');
+        variants.push_back(modifiedString);
+        modifiedString.push_back('2');
+        variants.push_back(modifiedString);
+        modifiedString.push_back('3');
+        variants.push_back(modifiedString);
+
+        // Add ! to end
+        modifiedString = variants[i];
+        modifiedString.push_back('!');
+        variants.push_back(modifiedString);
+    }
+    return;
+}
+
+void searchFromFileAndModify(std::string inputHash)
+{
+    std::string pwFilename;
+    std::cout << "Name of passwordlist file: ";
+    std::cin >> pwFilename;
+    std::cout << std::endl;
+
+    std::ifstream pwfile(pwFilename);
+    if (pwfile.is_open())
+    {
+        std::string tp;
+        std::vector<std::string> variants;
+        while (std::getline(pwfile, tp))
+        {
+
+            //Make variants and check them
+            generateVariants(tp, variants);
+            for (auto &variant : variants)
+            {
+                std::cout << variant << std::endl;
+                if (sha256(variant) == inputHash)
+                {
+                    std::cout << "Match found! Password is " << variant << std::endl;
+                    pwfile.close();
+                    return;
+                }
+            }
+            variants.clear();
+        }
+        std::cout << "No match based on initial guesses in file " << pwFilename << std::endl;
+        pwfile.close();
+        return;
+    }
+    std::cout << "Unable to read file " << pwFilename << std::endl;
+    return;
+}
+
 int main() {
     
     std::string hashIn = askInputHash();
     std::cout << "Hash to search for is " << hashIn << std::endl;
-    searchFromPasswordFile(hashIn);
+    searchFromFileAndModify(hashIn);
 
 
     // std::cout << sha256("1234567890_1") << std::endl;
